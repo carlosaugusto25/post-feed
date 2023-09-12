2cf6cf7cc45dab7f6e7fc03b765652755134d08a
Carlos Augusto <carlosaugusto@Mac-mini-de-Carlos.local>
2023-09-11 23:00:35 -0300

    finalizalçao do header e do sidebar da aplicação POST FEED

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Header } from "./components/Header.jsx"
+import style from './App.module.css';
+import './global.css';
+import { SideBar } from "./components/SideBar.jsx";
 
 export function App() {
 
   return (
     <>
       <Header />
+
+      <div className={style.wrapper}>
+        <SideBar />
+
+        <main>
+          
+        </main>
+
+      </div>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
+import { FaSignsPost } from 'react-icons/fa6';
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Post Feed</strong>
+            <FaSignsPost className={styles.icon} />
+            <p>Post Feed</p>
         </header>
     )
 }
